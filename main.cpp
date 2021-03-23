f5b034574d430605bb639db91e545999da9b3534
JUGisMUGlol <AtGum11@gmail.com>
2021-03-23 09:55:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main() {
+  string number, reversed_number = "";
+  
+  
+  cout << "Please enter the number or string you want to validate as a palindrome!" << endl;
+  cin >> number; //take input for number or string
+  
+  for (int i = number.length() - 1; i >= 0; i--) { //for loop to reverse the given input
+    reversed_number = reversed_number + number[i]; 
+  }
+  
+  if (reversed_number == number)
+     cout << "The provided number/string is a palindrome" << endl;
+  else 
+     cout << "The provided number or string is not a palindrome" << endl;
+  
+  return 0;
+}
